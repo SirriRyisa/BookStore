b3a0aeca42d71a62e8d647b03b22dd5500bceb00
SirriRyisa <sirriryisa@gmail.com>
2022-03-09 09:16:23 +0100

    Added an input branch to initialise the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import Input from './components/input'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi there Ryi</h1>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+      {/* <div className='input'></div>
+        <button>Add</button> */}
+    </div>
+  )
+}
+
+export default Input
